5bb1ad0b81df23c348d2c063dbb8e7fdbb206d78
Brandon Temba <brandontemba560@gmail.com>
2022-10-02 21:02:36 +0300

    commit tree 19a966bf35c88c2731ae93724bc19933557a7820

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,4 @@
 class PostsController < ApplicationController
-  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
     posts = Post.all
@@ -7,14 +6,11 @@ class PostsController < ApplicationController
   end
 
   def show
-    post = Post.find(params[:id])
-    render json: post
+    post = Post.find_by(id: params[:id])
+    if post
+      render json: post
+    else
+      render json: { errors: "Post not found" }
+    end
   end
-
-  private
-
-  def render_not_found_response
-    render json: { error: "Post not found" }, status: :not_found
-  end
-
 end
